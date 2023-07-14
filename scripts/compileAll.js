ab12eadbab2856026ccd132702b7bbc2487ed710
alanjoelschneider <alanjoelschneider@hotmail.com>
2023-07-14 00:56:32 -0300

    Create public and sub folders if they not exists before compiling

--- scripts/compileAll.js
+++ scripts/compileAll.js
@@ -1,5 +1,14 @@
+const path = require('path');
+const fs = require('fs');
+
 const compilePug = require('./compilePug');
 const compileLess = require('./compileLess');
+
+const rootDir = path.dirname(__dirname);
+
+// Create dirs if they not exist
+if (!fs.existsSync(path.join(rootDir, 'public'))) fs.mkdirSync(path.join(rootDir, 'public'));
+if (!fs.existsSync(path.join(rootDir, 'public', 'css'))) fs.mkdirSync(path.join(rootDir, 'public', 'css'));
 
 compileLess();
 compilePug();
